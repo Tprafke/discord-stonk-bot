583270b88fe65fceca7eac2ab9fe2263e4c5de07
toby <rtprafke96@gmail.com>
2021-03-11 21:56:33 -0500

    price-function

--- bot.js
+++ bot.js
@@ -2,8 +2,16 @@ const myToken = require('./config.js');
 const token = myToken.token;
 
 const Discord = require("discord.js");
-const fetch = require("node-fetch");
+const yahooStockPrices = require('yahoo-stock-prices');
 const client = new Discord.Client();
+
+const getPrice = async function (ticker) {
+    let stockPrice = await yahooStockPrices.getCurrentPrice(ticker).catch(e => { console.log(e) });
+    if (stockPrice) {
+        return stockPrice;
+    }
+    
+}
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -11,8 +19,15 @@ client.on("ready", () => {
 
 client.on("message", msg => {
     if (msg.author.bot) return
-    if (msg.content === "ping") {
-        msg.reply("pong");
+    if (msg.content === "Thank you Stonk Bot") {
+        msg.reply("You're welcome");
+    }
+    if (msg.content.includes("$")) {
+        let string = msg.content;
+        string = string.slice(1);
+        string = string.toUpperCase();
+        // getPrice(string).then(price => console.log(price));
+        getPrice(string).then(price => msg.channel.send(`$${price}`));
     }
 });
 
