6bc786c64d5edff21e8e53cbbdd55ac00bcd5d5f
toby <rtprafke96@gmail.com>
2021-03-19 21:14:39 -0400

    adjusted-charater-limit-and-response-object

--- bot.js
+++ bot.js
@@ -8,22 +8,27 @@ const client = new Discord.Client();
 const getPrice = async function (ticker) {
     
     let response;
-
-    // Format Ticker for API call
-    ticker = ticker.slice(1);
-    ticker = ticker.toUpperCase();
-    // Runs getCurrentPrice function - Returns stock price from yahoo finance
-    let stockPrice = await yahooStockPrices.getCurrentPrice(ticker).catch(e => { console.log(e) });
-    // Checks if a valid response has been recieved
-    if (stockPrice === undefined) {
+    // Checks if request exceeds 5 character limit
+    if (ticker.length <= 5) {
+        // Format Ticker for API call
+        ticker = ticker.slice(1);
+        ticker = ticker.toUpperCase();
+        // Runs getCurrentPrice function - Returns stock price from yahoo finance
+        let stockPrice = await yahooStockPrices.getCurrentPrice(ticker).catch(e => { console.log(e) });
+        // Checks if a valid response has been recieved
+        if (stockPrice === undefined) {
+            response = "Invalid Entry";
+            return response; 
+        } else {
+            let stockPage = `https://finance.yahoo.com/quote/${ticker}?p=${ticker}&.tsrc=fin-srch`;
+            response = `Ticker: ${ticker}\nLast Price: $${stockPrice}\nMore Info: ${stockPage}`
+            return response;
+        }
+    }
+    else {
         response = "Invalid Entry";
         return response; 
-    } else {
-        response = { price: `$${stockPrice}`, stockPage: `https://finance.yahoo.com/quote/${ticker}?p=${ticker}&.tsrc=fin-srch`};
-        response = `Ticker: ${ticker}\nLast Price: ${response.price}\nMore Info: ${response.stockPage}`
-        return response;
     }
-    
 }
 
 client.on("ready", () => {
@@ -32,10 +37,13 @@ client.on("ready", () => {
 // Checks discord client for new messages
 client.on("message", msg => {
     if (msg.author.bot) return
+    if (msg.content === "Hello Stonk Bot") {
+        msg.reply("Hello")
+    }
     if (msg.content === "Thank you Stonk Bot") {
         msg.reply("You're welcome");
     }
-    if (msg.content === "!stonks") {
+    if (msg.content === "!stonks" || msg.content === "!STONKS") {
         msg.channel.send({
             files: [
                 "./images/stonks.jpeg"
@@ -48,12 +56,9 @@ client.on("message", msg => {
     if (msg.content === "!GME" || msg.content === "!gme" || msg.content === "!AMC" || msg.content === "!amc" ) {
         msg.channel.send("We like the Stock!");
     }
-    if (msg.content === "Hello Stonk Bot") {
-        msg.reply("Hello")
-    }
     // Checks if message requested stock price
     // Formats string for yahoo finance request and returns ticker price
-    if (msg.content.includes("$") && msg.content.length <= 5) {
+    if (msg.content.includes("$") && msg.content.length <= 10) {
         let string = msg.content;
         getPrice(string).then(response => msg.channel.send(response));
     }
